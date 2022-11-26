1a8b42564aa609d7416357f0375b803eb59b7215
The Refrigerator <21050516+The-Frige@users.noreply.github.com>
2022-11-26 00:13:13 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,31 +1,55 @@
 #include <SFML/Graphics.hpp>
 #include <cmath>
 #include <iostream>
+#include <iomanip>
+#include <array>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <iostream>
 using namespace std;
 
 //Function prototypes
-void run(sf::RenderWindow&, const sf::VertexArray&, const sf::VertexArray&, const sf::VertexArray&, const sf::VertexArray&, const sf::VertexArray&, const sf::VertexArray&); //Window handling and rendering stuff onto the window
+void run(sf::RenderWindow&, const sf::VertexArray&, const sf::VertexArray&, const sf::VertexArray&, const sf::VertexArray&, const sf::VertexArray&, const sf::VertexArray&, const sf::VertexArray&, const sf::VertexArray&, const sf::VertexArray&, const sf::VertexArray&); //Window handling and rendering stuff onto the window
 //this run function below is an overloaded debug function to see if the grid even renders
 void run(sf::RenderWindow&, const sf::VertexArray&, const sf::VertexArray&, const sf::VertexArray&, const sf::VertexArray&, const sf::VertexArray&);
 
 double calculatorPlus(int, int, int); //Quadratic calculator
 double calculatorMinus(int, int, int); //Quadratic calculator
 double calculateY(int, int, int, int); //Used to calculate the y coordinate for graphing
+sf::VertexArray setPoints(const array<int, 5>&, const array<int, 5>&, const array<int, 5>&, int);
 sf::VertexArray setPoints(int); //Sets up vertex arrays to graph (This one's for the grid)
-array<int, 5> getInputs(string); //Function to get numbers for a, b, and c
+bool readFile(string, array<int, 5>&, array<int, 5>&, array<int, 5>&);
+
+
 
 int main() {
-	sf::VertexArray graph = setPoints(1);
-	sf::VertexArray graphlinesX = setPoints(2);
-	sf::VertexArray graphlinesX2 = setPoints(3);
-	sf::VertexArray graphlinesY = setPoints(4);
-	sf::VertexArray graphlinesY2 = setPoints(5);
+	array<int, 5> a_array = {0, 0, 0, 0, 0};
+	array<int, 5> b_array = {0, 0, 0, 0, 0};
+	array<int, 5> c_array = {0, 0, 0, 0, 0};
+	string filename("");
+
+	if (!readFile(filename, a_array, b_array, c_array))
+		return 1;
+
+
+	sf::VertexArray equation1 = setPoints(a_array, b_array, c_array, 0);
+	sf::VertexArray equation2 = setPoints(a_array, b_array, c_array, 1);
+	sf::VertexArray equation3 = setPoints(a_array, b_array, c_array, 2);
+	sf::VertexArray equation4 = setPoints(a_array, b_array, c_array, 3);
+	sf::VertexArray equation5 = setPoints(a_array, b_array, c_array, 4);
+
+	sf::VertexArray graph = setPoints(0);
+	sf::VertexArray graphlinesX = setPoints(1);
+	sf::VertexArray graphlinesX2 = setPoints(2);
+	sf::VertexArray graphlinesY = setPoints(3);
+	sf::VertexArray graphlinesY2 = setPoints(4);
 	sf::RenderWindow window(sf::VideoMode(300, 300), "Quadratic Calculator");
-	run(window, graph, graphlinesX, graphlinesX2, graphlinesY, graphlinesY2);
+	run(window, equation1, equation2, equation3, equation4, equation5, graph, graphlinesX, graphlinesX2, graphlinesY, graphlinesY2);
 	return 0;
 }
 
-void run(sf::RenderWindow& mWindow, const sf::VertexArray& graph, const sf::VertexArray& points, const sf::VertexArray& graphlinesX, const sf::VertexArray& graphlinesX2, const sf::VertexArray& graphlinesY, const sf::VertexArray& graphlinesY2)
+void run(sf::RenderWindow& mWindow, const sf::VertexArray& equation1, const sf::VertexArray& equation2, const sf::VertexArray& equation3, const sf::VertexArray& equation4, const sf::VertexArray& equation5, const sf::VertexArray& graph, const sf::VertexArray& graphlinesX, const sf::VertexArray& graphlinesX2, const sf::VertexArray& graphlinesY, const sf::VertexArray& graphlinesY2)
 {
 	sf::Event event;
 	sf::View view = mWindow.getDefaultView();
@@ -50,7 +74,11 @@ void run(sf::RenderWindow& mWindow, const sf::VertexArray& graph, const sf::Vert
 		}
 		mWindow.setView(view);
 		mWindow.clear(sf::Color::White);
-		mWindow.draw(points);
+		mWindow.draw(equation1);
+		mWindow.draw(equation2);
+		mWindow.draw(equation3);
+		mWindow.draw(equation4);
+		mWindow.draw(equation5);
 		mWindow.draw(graph);
 		mWindow.draw(graphlinesX);
 		mWindow.draw(graphlinesX2);
@@ -98,7 +126,7 @@ double calculatorPlus(int a, int b, int c)
 {
 	  double total;
 
-	  total = ((-b + (sqrt((pow(b,2)) - 4 * a * c))) / 2*a);
+	  total = (-b + sqrt((pow(b,2)) - (4 * a * c))) / (2*a);
 	  cout << "x = " << total << endl;
 	  return total;
 }
@@ -107,7 +135,7 @@ double calculatorMinus(int a, int b, int c)
 {
 	  double total;
 
-	  total = ((-b - (sqrt((pow(b,2)) - 4 * a * c))) / 2*a);
+	  total = (-b - sqrt((pow(b,2)) - (4 * a * c))) / (2*a);
 	  cout << "x = " << total << endl;
 	  return total;
 }
@@ -117,27 +145,12 @@ double calculateY(int a, int b, int c, int x)
 	return -(a * pow(x, 2)) + (b*x)+ c;
 }
 
-sf::VertexArray setPoints(int type) //0 is the quadratic equation, 1 is the red graph lines, 2 is linesx, 3 is linesx2, 4 is linesy, 5 is linesy2
+sf::VertexArray setPoints(int type) //0 is the red graph lines, 1 is linesx, 2 is linesx2, 3 is linesy, 4 is linesy2
 {
 	double x = 0, y = 0;
 	switch(type)
 	{
 	case 0:
-	{
-		/*
-		sf::VertexArray points(sf::LinesStrip, 21.0f);
-        for (int counter = 0; counter < 21; counter++)
-        {
-        	x = static_cast<float>(counter) - ((21-1)/2);
-        	y = -(a * pow(x, 2)) + (b*x)+ c;
-        	points[counter].position = sf::Vector2f(x+150.0f, y+150.0f);
-        	points[counter].color = sf::Color::Black;
-        }
-
-		*/
-		break;
-	}
-	case 1:
 	{
 		sf::VertexArray graph(sf::Lines, 4);
 		graph[0].position = sf::Vector2f(0, 150); //-600, 150 //These are the cordinates for stuff that can be zoomed out
@@ -152,7 +165,7 @@ sf::VertexArray setPoints(int type) //0 is the quadratic equation, 1 is the red 
 		break;
 	}
 
-	case 2:
+	case 1:
 	{
 		sf::VertexArray graphlinesX(sf::Lines, 301);
         for (int counterX = 0; counterX < 301; counterX++)
@@ -175,7 +188,7 @@ sf::VertexArray setPoints(int type) //0 is the quadratic equation, 1 is the red 
         return graphlinesX;
 		break;
 	}
-	case 3:
+	case 2:
 	{
 		sf::VertexArray graphlinesX2(sf::Lines, 301);
         for (int counterX = 0; counterX < 301; counterX++)
@@ -201,7 +214,7 @@ sf::VertexArray setPoints(int type) //0 is the quadratic equation, 1 is the red 
         return graphlinesX2;
         break;
 	}
-	case 4:
+	case 3:
 	{
 		sf::VertexArray graphlinesY(sf::Lines, 301);
         for (int counterY = 0; counterY < 301; counterY++)
@@ -224,7 +237,7 @@ sf::VertexArray setPoints(int type) //0 is the quadratic equation, 1 is the red 
         return graphlinesY;
         break;
 	}
-	case 5:
+	case 4:
 	{
 		sf::VertexArray graphlinesY2(sf::Lines, 301);
         for (int counterY = 0; counterY < 301; counterY++)
@@ -252,4 +265,181 @@ sf::VertexArray setPoints(int type) //0 is the quadratic equation, 1 is the red 
 	}
 	}
 }
+
+sf::VertexArray setPoints(const array<int, 5>& a_array, const array<int, 5>& b_array, const array<int, 5>& c_array, int count)
+{
+	int a = 0, b = 0, c = 0;
+	sf::VertexArray points(sf::LineStrip, 21.0f);
+	a = a_array[count];
+	b = b_array[count];
+	c = c_array[count];
+	switch (count)
+	{
+	case 0:
+	{
+	    for (int counter = 0; counter < 21; counter++)
+	    {
+	    	double x = 0, y = 0;
+	    	x = static_cast<double>(counter) - ((21-1)/2);
+	    	y = calculateY(a, b, c, x);
+	    	points[counter].position = sf::Vector2f(x+150.0f, y+150.0f);
+	    	points[counter].color = sf::Color::Black;
+	    }
+	    return points;
+		break;
+	}
+	case 1:
+	{
+	    for (int counter = 0; counter < 21; counter++)
+	    {
+	    	double x = 0, y = 0;
+	    	x = static_cast<double>(counter) - ((21-1)/2);
+	    	y = calculateY(a, b, c, x);
+	    	points[counter].position = sf::Vector2f(x+150.0f, y+150.0f);
+	    	points[counter].color = sf::Color::Red;
+	    }
+	    return points;
+		break;
+	}
+	case 2:
+	{
+	    for (int counter = 0; counter < 21; counter++)
+	    {
+	    	double x = 0, y = 0;
+	    	x = static_cast<double>(counter) - ((21-1)/2);
+	    	y = calculateY(a, b, c, x);
+	    	points[counter].position = sf::Vector2f(x+150.0f, y+150.0f);
+	    	points[counter].color = sf::Color::Blue;
+	    }
+	    return points;
+		break;
+	}
+	case 3:
+	{
+	    for (int counter = 0; counter < 21; counter++)
+	    {
+	    	double x = 0, y = 0;
+	    	x = static_cast<double>(counter) - ((21-1)/2);
+	    	y = calculateY(a, b, c, x);
+	    	points[counter].position = sf::Vector2f(x+150.0f, y+150.0f);
+	    	points[counter].color = sf::Color::Green;
+	    }
+	    return points;
+		break;
+	}
+	case 4:
+	{
+	    for (int counter = 0; counter < 21; counter++)
+	    {
+	    	double x = 0, y = 0;
+	    	x = static_cast<double>(counter) - ((21-1)/2);
+	    	y = calculateY(a, b, c, x);
+	    	points[counter].position = sf::Vector2f(x+150.0f, y+150.0f);
+	    	points[counter].color = sf::Color::Magenta;
+	    }
+	    return points;
+		break;
+	}
+	}
+
+}
+
+bool readFile(string filename, array<int, 5>& a_array, array<int, 5>& b_array, array<int, 5>& c_array)
+{
+	ifstream inputFile;
+	ofstream outputFile;
+	string equation, temp;
+	int count = 0;
+
+	string terms1, terms2, terms3, terms4, terms5;
+	int tempa, tempb, tempc, temp0;
+
+	stringstream ss;
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	double solution1, solution2;
+
+	cout << "Enter filename:";
+	cin >> filename;
+	inputFile.open(filename);
+	if (!inputFile)
+	{
+		cout << "Error no file shutting down...";
+		return false;
+	}
+	while(getline(inputFile, equation))
+	{
+		ss.str(string());
+		ss.clear();
+		cout << equation << endl;
+		ss << equation;
+
+
+		while (ss) {
+
+			ss >> tempa >> terms1 >> terms2 >> tempb >> terms3 >> terms4 >> tempc >> terms5 >> temp0;
+
+			if (terms2 != "+" && terms2 != "-")
+			{
+				cout << "Error! Refusing to read equation due to incorrect format...\n";
+				continue;
+			}
+			if (terms4 != "+" && terms4 != "-")
+			{
+				cout << "Error! Refusing to read equation due to incorrect format...\n";
+				continue;
+			}
+			if (terms5 != "=")
+			{
+				cout << "Error! Refusing to read equation due to incorrect format...\n";
+				continue;
+			}
+			if (terms1 != "x^2")
+			{
+				cout << "Error! Refusing to read equation due to incorrect format...\n";
+				continue;
+			}
+			if (terms3 != "x")
+			{
+				cout << "Error! Refusing to read equation due to incorrect format...\n";
+				continue;
+			}
+			if (temp0 != 0)
+			{
+				cout << "Error! Refusing to read equation due to incorrect format...\n";
+				continue;
+			}
+
+			a = tempa;
+			b = tempb;
+			c = tempc;
+
+			a_array[count] = a;
+			b_array[count] = b;
+			c_array[count] = c;
+
+
+		}
+		count++;
+		solution1 = calculatorPlus(a,b,c);
+		solution2 = calculatorMinus(a,b,c);
+		cout << "Numbers\n";
+		cout << "a: " << a << endl;
+		cout << "b: " << b << endl;
+		cout << "c: " << c << endl;
+		cout << "Solutions\n";
+		cout << "x: " << solution1 << endl;
+		cout << "x: " << solution2 << endl;
+		cout << endl;
+
+	}
+
+
+	inputFile.close();
+	return true;
+
+
+}
+
 
